f59591f013823dac5212de4cf19df301a281f5dd
Đinh Ngọc Tâm <tamdinh572003@gmail.com>
2023-08-18 18:04:24 +0700

    commit

---
+++ portfolio/main.js
@@ -0,0 +1,19 @@
+console.log('Hello');
+
+
+var array =[1,2,3,4];
+
+
+for(var item of array){
+    console.log(item);
+}
+
+var person = {
+    name : "Tam",
+    
+}
+
+
+for(var item in person){
+    console.log(person.name);
+}
